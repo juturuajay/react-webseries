37e72cc1f7a768485a5f1d675d048056e4cea8aa
juturuajay <86615742+juturuajay@users.noreply.github.com>
2023-06-01 23:31:45 +0530

    Episode-02

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+
 const heading = React.createElement("div", {
     id: "parent"},[React.createElement("div", {
         id: "child1"},[React.createElement("h1", {
-        },"I am an H1 tag"),React.createElement("h1", {
+        },"I am an H1 tag"),React.createElement("h2", {
         },"I am an H2 tag")]),React.createElement("div", {
             id: "child2"},[React.createElement("h1", {
-            },"I am an H1 tag"),React.createElement("h1", {
+            },"I am an H1 tag"),React.createElement("h2", {
             },"I am an H2 tag")])]);
 
 
-    console.log(heading); //object
+ 
 const root = ReactDOM.createRoot(document.getElementById("headerr"));
 
 root.render(heading)
