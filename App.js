197940ed7d2832f4df4da9aa2c02d3b624f936c9
juturuajay <86615742+juturuajay@users.noreply.github.com>
2023-06-03 10:36:59 +0530

    episode-03

--- App.js
+++ App.js
@@ -1,18 +1,27 @@
 import React from "react";
 import ReactDOM from 'react-dom/client'
 
+const elem = <span>Hello span</span>
 
-const heading = React.createElement("div", {
-    id: "parent"},[React.createElement("div", {
-        id: "child1"},[React.createElement("h1", {
-        },"I am an H1 tag"),React.createElement("h2", {
-        },"I am an H2 tag")]),React.createElement("div", {
-            id: "child2"},[React.createElement("h1", {
-            },"I am an H1 tag"),React.createElement("h2", {
-            },"I am an H2 tag")])]);
+const jsxheading = (
+    <div>
+       <h1>{elem}</h1>
+       <h1>Hello JSX using react element</h1>
+       
+    </div>
+   )
+
+const FunctionalComponent = () => (
+   <div id="container">
+       {jsxheading}
+        <h1>Hello FunctionalComponent</h1>
+        <h2>Hello F2</h2>
+    </div>
+)
 
 
- 
-const root = ReactDOM.createRoot(document.getElementById("headerr"));
 
-root.render(heading)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<FunctionalComponent />)
